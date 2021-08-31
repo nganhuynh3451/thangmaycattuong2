2ba5a014a39126840c803121edc8b63294de4581
nganhuynh3451 <ngan.huynh.3451@gmail.com>
2021-08-31 15:05:01 +0700

    modified:   img/logo.png 	modified:   index.html 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -4,4 +4,14 @@ const menuLinks = document.querySelector('.nav-menu');
 menu.addEventListener('click', function() {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
-})
+});
+
+const scrollToTop = document.querySelector('.scroll-to-top');
+
+window.addEventListener('scroll', () => {
+    if (window.pageYOffset > 100) {
+        scrollToTop.classList.add('active');
+    } else {
+        scrollToTop.classList.remove('active');
+    }
+});
