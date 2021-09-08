63336f20c82d4d6e08a8284bfe53c405b8c7d701
Ngân Huỳnh <86921746+nganhuynh3451@users.noreply.github.com>
2021-09-08 21:06:59 +0700

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,20 @@ const scrollToTop = document.querySelector('.scroll-to-top');
 scrollToTop.addEventListener('click', function () {
     window.scrollTo (0, 0);
 });
+scrollToTop.addEventListener('scroll', () => {
+    if (window.pageYOffset > 0) {
+        scrollToTop.classList.add('active');
+    } else {
+        scrollToTop.classList.remove('active');
+    }
+}); 
+
+const navbar = document.querySelector('.navbar');
+
+navbar.addEventListener('scroll', () => {
+    if (window.pageYOffset > 80) {
+        navbar.classList.add('active');
+    } else {
+        navbar.classList.remove('active');
+    }
+}); 
